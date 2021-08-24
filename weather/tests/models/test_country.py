7406ade9af5bbbde56e9fb8edcb1c461c9790b18
Arthuchaut <arthuchaut@gmail.com>
2021-08-24 22:53:35 +0200

    [Cleaning] Cleaned model test code

--- weather/tests/models/test_country.py
+++ weather/tests/models/test_country.py
@@ -5,11 +5,11 @@ from weather.models import Country
 
 class TestCountry:
     def test_init(self, country_fake_data: dict[str, Any]) -> None:
-        self._country: Country = Country(**country_fake_data)
+        country: Country = Country(**country_fake_data)
 
         for field, value in country_fake_data.items():
-            assert hasattr(self._country, field)
-            assert isinstance(getattr(self._country, field), type(value))
+            assert hasattr(country, field)
+            assert isinstance(getattr(country, field), type(value))
 
     @pytest.mark.django_db
     def test_creation(self, country_fake_data: dict[str, Any]) -> None:
