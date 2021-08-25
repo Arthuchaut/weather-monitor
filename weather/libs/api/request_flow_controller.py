e3d5f7a201043053a8b1547a4068e021c2e09297
Arthuchaut <arthuchaut@gmail.com>
2021-08-25 23:28:31 +0200

    [Feature] Added the request flow controller

---
+++ weather/libs/api/request_flow_controller.py
@@ -0,0 +1,105 @@
+import pathlib
+import time
+
+
+class RequestFlowController:
+    def __init__(
+        self,
+        flow_capacity: int,
+        time_range: int,
+        state_file: pathlib.Path = None,
+    ) -> None:
+        '''The constructor.
+
+        Args:
+            flow_capacity (int): The maximum request allowed
+                in the given time range.
+            time_range (int): The time range (in seconds).
+            state_file (pathlib.Path): The file that used to
+                store the flow state.
+        '''
+
+        self._flow_capacity: int = flow_capacity
+        self._time_range: int = time_range
+        self._ref_timestamp: float = None
+        self._req_count: int = 0
+        self._state_file: pathlib.Path = state_file
+
+        if self._state_file:
+            self._recover_state()
+
+    def _recover_state(self) -> None:
+        '''Recover the flow state from the given file.
+        If the file is empty, pass.
+
+        Raises:
+            StateRecoveryError: If no file is given or
+                if data is corrupted.
+        '''
+
+        if not self._state_file:
+            raise StateRecoveryError('No state file given.')
+
+        if not self._state_file.exists():
+            self._state_file.touch()
+
+        if states := self._state_file.read_text().split():
+            try:
+                self._ref_timestamp = float(states[0])
+                self._req_count = int(states[1])
+            except (IndexError, ValueError):
+                self._ref_timestamp = None
+                self._req_count = 0
+
+                raise StateRecoveryError(
+                    'The file data seems to be corrupted.'
+                )
+
+    def _save_state(self) -> None:
+        '''Save the self._ref_timestamp and self._req_count attributes
+        to the state file.
+
+        Raises:
+            StateRecoveryError: If no state file is given.
+        '''
+
+        if not self._state_file:
+            raise StateRecoveryError('No state file given.')
+
+        self._state_file.write_text(f'{self._ref_timestamp} {self._req_count}')
+
+    def wait_for_free_flow(self) -> None:
+        '''Update the flow state and wait until the flow
+        is free.
+        Save the current state if the state file is given.
+        '''
+
+        if not self._ref_timestamp:
+            self._ref_timestamp = time.time()
+
+        self._req_count += 1
+        cur_timestamp: float = time.time()
+
+        if (secs := cur_timestamp - self._ref_timestamp) < self._time_range:
+            if self._req_count < self._flow_capacity:
+                if self._state_file:
+                    self._save_state()
+                return
+
+            time.sleep(self._time_range - secs)
+            self._req_count = 1
+
+            if self._state_file:
+                self._save_state()
+
+            return
+
+        self._req_count = 1
+        self._ref_timestamp = time.time()
+
+        if self._state_file:
+            self._save_state()
+
+
+class StateRecoveryError(Exception):
+    ...
---
+++ weather/tests/libs/api/unit/test_request_flow_controller.py
@@ -0,0 +1,93 @@
+import pathlib
+import time
+import pytest
+from weather.libs.api.request_flow_controller import (
+    RequestFlowController,
+    StateRecoveryError,
+)
+
+
+class TestRequestFlowController:
+    def test_init(self, tmp_path: pathlib.Path) -> None:
+        state_file: pathlib.Path = tmp_path / '.owm_flow_state'
+        req_flow: RequestFlowController = RequestFlowController(
+            state_file=state_file,
+            flow_capacity=600,
+            time_range=60,
+        )
+        assert req_flow._state_file == state_file
+        assert req_flow._flow_capacity == 600
+        assert req_flow._time_range == 60
+        assert hasattr(req_flow, '_ref_timestamp')
+        assert hasattr(req_flow, '_req_count')
+
+    @pytest.mark.parametrize(
+        'should_raise, state_data',
+        [
+            (False, '1629923065.9253566 10'),
+            (True, 'hello 1629923065.9253566'),
+            (True, '1629923065.9253566'),
+            (False, ''),
+        ],
+    )
+    def test__recover_state(
+        self, should_raise: bool, state_data: str, tmp_path: pathlib.Path
+    ) -> None:
+        state_file: pathlib.Path = tmp_path / '.owm_flow_state'
+        state_file.write_text(state_data)
+        req_flow: RequestFlowController = RequestFlowController(
+            flow_capacity=600,
+            time_range=60,
+        )
+
+        assert req_flow._ref_timestamp == None
+        assert req_flow._req_count == 0
+
+        req_flow._state_file = state_file
+
+        if should_raise:
+            with pytest.raises(StateRecoveryError):
+                req_flow._recover_state()
+
+            assert req_flow._ref_timestamp == None
+            assert req_flow._req_count == 0
+        else:
+            req_flow._recover_state()
+
+            if state_data:
+                assert req_flow._ref_timestamp == 1629923065.9253566
+                assert req_flow._req_count == 10
+
+    def test__save_state(self, tmp_path: pathlib.Path) -> None:
+        state_file: pathlib.Path = tmp_path / '.owm_flow_state'
+        req_flow: RequestFlowController = RequestFlowController(
+            state_file=state_file,
+            flow_capacity=600,
+            time_range=60,
+        )
+        req_flow._ref_timestamp = 1629923065.9253566
+        req_flow._req_count = 10
+        req_flow._save_state()
+
+        assert state_file.read_text() == '1629923065.9253566 10'
+
+        req_flow._state_file = None
+
+        with pytest.raises(StateRecoveryError):
+            req_flow._save_state()
+
+    def test_wait_for_free_flow(self, tmp_path: pathlib.Path) -> None:
+        state_file: pathlib.Path = tmp_path / '.owm_flow_state'
+        req_flow: RequestFlowController = RequestFlowController(
+            state_file=state_file,
+            flow_capacity=2,
+            time_range=1,
+        )
+        req_flow._ref_timestamp = time.time()
+        begin_time: float = time.time()
+
+        for _ in range(4):
+            req_flow.wait_for_free_flow()
+            time.sleep(0.1)
+
+        assert int(time.time() - begin_time) == 2
